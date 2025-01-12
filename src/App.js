fcf437d4aefacb157f5bda2921d7ba27b1191444
josie-espiritu <espiritu.josiemar@gmail.com>
2025-01-12 19:48:53 +0800

    modified app.js

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Products from './components/Products';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Section from './components/Section';
+import Homepage from './components/Homepage';
 
 
 
@@ -12,7 +13,7 @@ function App() {
   return (
     <>
       <nav><Navigation/></nav>
-      
+      <Homepage/>
       <footer>
         <Footer/>
       </footer>
